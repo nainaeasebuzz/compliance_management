975f2fca5cb039f656e54c6c9c0990ec5d8ec1fd
nainaeasebuzz <naina.jain@easebuzz.in>
2025-07-22 17:36:36 +0530

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def login_view(request):
+    return render(request, 'main/login.html')
+
+def landing_page(request):
+    return render(request, 'main/landing.html')
